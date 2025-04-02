7e33c080176375629fe0e987bb70694c8da525eb
deepak-official-account <bisht.deepak@thinksys.com>
2025-04-02 18:09:13 +0530

    initial commit

---
+++ Javascript/Intro.js
@@ -0,0 +1,12 @@
+// all negative and positive values are true
+//! Js Returns undefined when no return keyword is used inside a method
+//! - symbol not works in case of operator overloading 
+//? All browsers uses V8 engine for js
+//? Js is not a compiled language
+//? Js is a interpreted language
+// hoisiting works for only var 
+
+console.log(3);
+console.log(-6);
+
+
