992d9e2630dc8c6b428021070177703a2e3eeeae
deepak-official-account <bisht.deepak@thinksys.com>
2025-04-03 10:31:13 +0530

    commit files 1

--- Javascript/Intro.js
+++ Javascript/Intro.js
@@ -1,12 +1,17 @@
 // all negative and positive values are true
 //! Js Returns undefined when no return keyword is used inside a method
 //! - symbol not works in case of operator overloading 
+// ! we can use (!) Symbol to convert any data type to boolean
 //? All browsers uses V8 engine for js
 //? Js is not a compiled language
 //? Js is a interpreted language
 // hoisiting works for only var 
 
-console.log(3);
-console.log(-6);
-
+//todo: Variables
+var _name="Deepak";
+var $company="thinksys";
+// var 1role=".Net Developer"; // error
+console.log(_name);
+console.log($company);
+// console.log(1role);
 
---
+++ TrainingFiles/operator.js
@@ -0,0 +1,57 @@
+// Arithmetic Operators
+
+var a = 0;
+console.log("a++", a++);
+
+var a = 0;
+console.log("++a", ++a);
+
+var a = 0;
+console.log("a--", a--);
+
+var a = 0;
+console.log("--a", --a);
+
+
+console.log(1 + 1);
+
+console.log("1" + 1);
+
+console.log(1 + "a"); 
+
+console.log("1" + "1");
+
+console.log("a" - "b");
+
+
+// Comparison Operators
+
+ console.log(1 == 1); 
+
+ console.log(1 == "1");
+
+ console.log(1 === "1");
+
+ console.log(1 !== "1");
+
+console.log(1 && 0);
+
+
+console.log(11 && 12);
+
+console.log(21 || 23);
+
+console.log(!!null);
+
+
+console.log(!0)
+
+console.log(!-1)
+
+console.log(!!1)
+
+console.log(!!-1)
+
+
+
+
